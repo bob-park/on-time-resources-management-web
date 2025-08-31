43537646ba8394f6a7f2333d54eaf39624fa0297
Bob Park <hwpark11@kakao.com>
2025-08-31 15:09:41 +0900

    Initialize project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,10 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// eslint.config.mjs
+import eslintConfig from '@bob-park/eslint-config-bobpark';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { defineConfig } from 'eslint/config';
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default defineConfig([
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    extends: [eslintConfig],
   },
-];
-
-export default eslintConfig;
+]);
