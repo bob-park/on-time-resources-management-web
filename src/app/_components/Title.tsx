56fba1fbac6de5f2d25f9c49bde0f4af9fc00dc8
Bob Park <hwpark11@kakao.com>
2025-09-04 22:09:22 +0900

    feat: 먼가 함

--- src/app/_components/Title.tsx
+++ src/app/_components/Title.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Title() {
   return (
     <Link className="btn btn-ghost rounded-full" href="/">
-      <span className="bg-gradient-to-r from-white via-gray-500 to-gray-700 bg-clip-text text-xl font-bold text-transparent">
+      <span className="bg-gradient-to-r from-white via-gray-300 to-gray-400 bg-clip-text text-xl font-bold text-transparent">
         사내 기기 관리
       </span>
     </Link>
--- src/app/devices/_components/DeviceSimpleResult.tsx
+++ src/app/devices/_components/DeviceSimpleResult.tsx
@@ -1,5 +1,10 @@
 'use client';
 
+import { FaCircle, FaRegHandPaper } from 'react-icons/fa';
+import { GiBrokenPottery } from 'react-icons/gi';
+import { LuLaptop } from 'react-icons/lu';
+import { TiSpannerOutline } from 'react-icons/ti';
+
 import { useDeviceDashboard } from '@/domain/devices/queries/devices';
 
 export default function DeviceSimpleResult() {
@@ -13,40 +18,54 @@ export default function DeviceSimpleResult() {
         <div className="flex flex-row items-center gap-4">
           <div className="card bg-base-300 card-md flex-1 shadow-sm">
             <div className="card-body">
-              <h2 className="card-title text-gray-400">총 기기</h2>
-              <div className="flex w-full flex-col gap-3">
+              <h2 className="card-title text-gray-400">
+                <FaCircle className="size-5" /> 총 기기
+              </h2>
+              <div className="flex w-full flex-row items-center gap-3">
                 <p className="text-4xl font-bold">{dashboard?.total || 0}</p>
               </div>
             </div>
           </div>
           <div className="card bg-base-300 card-md flex-1 shadow-sm">
             <div className="card-body">
-              <h2 className="card-title text-gray-400">사용</h2>
-              <div className="flex w-full flex-col gap-3">
+              <h2 className="card-title text-gray-400">
+                <LuLaptop className="size-5" />
+                사용
+              </h2>
+              <div className="flex w-full flex-row items-center gap-3">
                 <p className="text-4xl font-bold">{dashboard?.status.used || 0}</p>
               </div>
             </div>
           </div>
           <div className="card bg-base-300 card-md flex-1 shadow-sm">
             <div className="card-body">
-              <h2 className="card-title text-gray-400">대기</h2>
-              <div className="flex w-full flex-col gap-3">
+              <h2 className="card-title text-gray-400">
+                <FaRegHandPaper className="size-5" />
+                대기
+              </h2>
+              <div className="flex w-full flex-row items-center gap-3">
                 <p className="text-4xl font-bold">{dashboard?.status.waiting || 0}</p>
               </div>
             </div>
           </div>
           <div className="card bg-base-300 card-md flex-1 shadow-sm">
             <div className="card-body">
-              <h2 className="card-title text-gray-400">고장</h2>
-              <div className="flex w-full flex-col gap-3">
+              <h2 className="card-title text-gray-400">
+                <GiBrokenPottery className="size-5" />
+                고장
+              </h2>
+              <div className="flex w-full flex-row items-center gap-3">
                 <p className="text-4xl font-bold">{dashboard?.status.broken || 0}</p>
               </div>
             </div>
           </div>
           <div className="card bg-base-300 card-md flex-1 shadow-sm">
             <div className="card-body">
-              <h2 className="card-title text-gray-400">수리</h2>
-              <div className="flex w-full flex-col gap-3">
+              <h2 className="card-title text-gray-400">
+                <TiSpannerOutline className="size-5" />
+                수리
+              </h2>
+              <div className="flex w-full flex-row items-center gap-3">
                 <p className="text-4xl font-bold">{dashboard?.status.repairing || 0}</p>
               </div>
             </div>
